40cd1461231b2fec045f8f2f101f566b92264464
Bui Hai <buihai2408@gmail.com>
2024-09-21 04:27:25 +0700

    them xoa sua the loai

---
+++ buihai/del_category.php
@@ -0,0 +1,12 @@
+<?php
+require '../db.php';
+if (isset($_GET['id'])) {
+
+    $ma_tgia = $_GET['id'];
+
+    $sql = "DELETE FROM theloai WHERE ma_tloai = {$_GET['id']}";
+    $conn->query($sql);
+
+    header('Location:category.php');
+}
+?>
